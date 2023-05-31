5894ee9f16e4d07b2713c031e7180ba612f212c7
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-05-31 14:36:40 +0000

    log 追加

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -1,23 +1,29 @@
-describe('Example', () => {
+describe("Example", () => {
   beforeAll(async () => {
+    console.log("before launchApp");
     await device.launchApp();
+    console.log("after launchApp");
   });
 
   beforeEach(async () => {
+    console.log("before reloadReactNative");
     await device.reloadReactNative();
+    console.log("after reloadReactNative");
   });
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
+  it("should have welcome screen", async () => {
+    console.log("before expect");
+    await expect(element(by.id("welcome"))).toBeVisible();
+    console.log("after expect");
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it("should show hello screen after tap", async () => {
+    await element(by.id("hello_button")).tap();
+    await expect(element(by.text("Hello!!!"))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it("should show world screen after tap", async () => {
+    await element(by.id("world_button")).tap();
+    await expect(element(by.text("World!!!"))).toBeVisible();
   });
 });

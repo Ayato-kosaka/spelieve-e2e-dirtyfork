29101baf6c9f2011c1e9098f15172e08dc821e17
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-06-03 04:22:02 +0000

    fetchSimulatorBuild を android apk に対応

--- .detoxrc.js
+++ .detoxrc.js
@@ -24,14 +24,14 @@ module.exports = {
     },
     "android.debug": {
       type: "android.apk",
-      binaryPath: "android/app/build/outputs/apk/debug/app-debug.apk",
+      binaryPath: "Spelieve.apk",
       build:
         "cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug",
       reversePorts: [8081],
     },
     "android.release": {
       type: "android.apk",
-      binaryPath: "android/app/build/outputs/apk/release/app-release.apk",
+      binaryPath: "Spelieve.apk",
       build:
         "cd android && ./gradlew assembleRelease assembleAndroidTest -DtestBuildType=release",
     },
--- fetchSimulatorBuild.js
+++ fetchSimulatorBuild.js
@@ -10,49 +10,70 @@
  * @name fetchSimulatorBuild.js
  */
 
-const axios = require('axios');
-const path = require('path');
-const { existsSync, writeFileSync } = require('fs');
-const { execSync } = require('child_process');
+const axios = require("axios");
+const path = require("path");
+const { existsSync, writeFileSync } = require("fs");
+const { execSync } = require("child_process");
+const app = require("./app.json");
 
-const OUTPUT_TAR_PATH = path.resolve(__dirname, 'Sample.tar.gz');
-const OUTPUT_SIMULATOR_BUILD_BINARY_PATH = path.resolve(__dirname, '.');
+const OUTPUT_SIMULATOR_BUILD_BINARY_PATH = path.resolve(__dirname, ".");
 
 // 最新のシミュレータ用ビルドファイルを取得する
 const getLatestSimulatorBuildBinary = async (platform) => {
-	if (existsSync(`${OUTPUT_SIMULATOR_BUILD_BINARY_PATH}/Sample.app`)) {
-		return;
-	}
+  console.log("Start getLatestSimulatorBuildBinary with platform:", platform);
 
-	try {
-		// 最新のシミュレータ用ビルドの結果リストを取得する
-		const buildJson = execSync(
-			`eas build:list --status finished --platform ${platform} --distribution simulator --non-interactive --json`,
-		);
-		// 最新のシミュレータ用ビルドファイルのURLを取得
-		const buildUrl = JSON.parse(buildJson.toString())[0]?.artifacts?.buildUrl;
-		const response = await fetchBinary(buildUrl);
-		writeFileSync(OUTPUT_TAR_PATH, response.data);
+  if (
+    existsSync(
+      `${OUTPUT_SIMULATOR_BUILD_BINARY_PATH}/${app.expo.slug}.${
+        platform === "ios" ? "app" : "apk"
+      }`
+    )
+  ) {
+    console.log("既にバイナリの取得が完了しています");
+    return;
+  }
 
-		if (!buildUrl) {
-			throw new Error('simulator buildしたバイナリが存在しません。simulator buildを実行して、再度試してください。');
-		}
+  try {
+    // 最新のシミュレータ用ビルドの結果リストを取得する
+    const buildJson = execSync(
+      `eas build:list --status finished --platform ${platform} --buildProfile preview-simulator --non-interactive --json`
+    );
+    // 最新のシミュレータ用ビルドファイルのURLを取得
+    const buildUrl = JSON.parse(buildJson.toString())[0]?.artifacts?.buildUrl;
+    if (!buildUrl) {
+      throw new Error(
+        "preview-simulator buildしたバイナリが存在しません。preview-simulator buildを実行して、再度試してください。"
+      );
+    }
 
-		// Sample.appファイルに展開する
-		execSync(`tar -xf ${OUTPUT_TAR_PATH} -C ${OUTPUT_SIMULATOR_BUILD_BINARY_PATH} && rm -rf ${OUTPUT_TAR_PATH}`);
+    const OUTPUT_TAR_PATH = path.resolve(
+      __dirname,
+      platform === "ios" ? "tmp.tar.gz" : `${app.expo.slug}.apk`
+    );
+    const response = await fetchBinary(buildUrl);
+    console.log(OUTPUT_TAR_PATH);
+    writeFileSync(OUTPUT_TAR_PATH, response.data);
 
-		return console.log('success: バイナリの取得が完了しました。');
-	} catch (e) {
-		throw new Error(`error log: ${e}`);
-	}
+    if (platform === "ios") {
+      // 〇〇.appファイルに展開する
+      execSync(
+        `tar -xf ${OUTPUT_TAR_PATH} -C ${OUTPUT_SIMULATOR_BUILD_BINARY_PATH}`
+      );
+      execSync(`rm -rf ${OUTPUT_TAR_PATH}`);
+    }
+
+    return console.log("success: バイナリの取得が完了しました。");
+  } catch (e) {
+    throw new Error(`error log: ${e}`);
+  }
 };
 
 const fetchBinary = async (url) => {
-	return axios.get(url, {
-		responseType: 'arraybuffer',
-		headers: { Accept: 'application/x-tar' },
-	});
+  return axios.get(url, {
+    responseType: "arraybuffer",
+    headers: { Accept: "application/x-tar" },
+  });
 };
 
-// 一旦iOSのみ
-(async () => await getLatestSimulatorBuildBinary('ios'))();
+(async () => await getLatestSimulatorBuildBinary("ios"))();
+(async () => await getLatestSimulatorBuildBinary("android"))();

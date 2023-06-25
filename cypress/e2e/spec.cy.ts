1cd0e877bc259a2509642af32ee2ea1f06b94d58
Ayato-kosaka <kosaka.ayato@gmail.com>
2023-06-25 08:36:45 +0000

    cypress 関連を削除

--- cypress/e2e/spec.cy.ts
+++
@@ -1,8 +0,0 @@
-describe("spec.cy.ts", () => {
-  it("should pass", () => {
-    cy.visit("http://localhost:19006/");
-    cy.screenshot();
-    expect(true).to.equal(true);
-    cy.screenshot();
-  });
-});
